fe9728b73393b3e63c803904a0e1e22007dcd4bc
Bibek <bbeksh@gmail.com>
2019-02-15 01:07:09 -0600

    Collection view working. But selecting the image and indefinite scroll is not working

---
+++ ImageCollectionView/Cell.swift
@@ -0,0 +1,42 @@
+//
+//  Cell.swift
+//  ImageCollectionView
+//
+//  Created by Bibek Shrestha on 2/14/19.
+//  Copyright © 2019 Bibek Shrestha. All rights reserved.
+//
+
+import UIKit
+
+class Cell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    
+    override var bounds: CGRect {
+        didSet {
+            self.layoutIfNeeded()
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        self.imageView.layer.masksToBounds = true
+    }
+
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        self.setCircularImageView()
+    }
+    
+    func setCircularImageView() {
+        self.imageView.layer.cornerRadius = CGFloat(roundf(Float(self.imageView.frame.size.width / 2.0)))
+    }
+    
+    
+    
+}
